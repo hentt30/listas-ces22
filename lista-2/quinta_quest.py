a464976d9558546cfae7e7e4de0a4e285c315797
Henrique <hentt30@gmail.com>
2020-05-01 17:41:25 -0400

    quinta questão adicionada

---
+++ lista-2/quinta_quest.py
@@ -0,0 +1,17 @@
+class Point:
+    """ Create a new Point, at coordinates x, y """
+
+    def __init__(self, x=0, y=0):
+        """ Create a new point at x, y """
+        self.x = x
+        self.y = y
+
+    def distance_from_origin(self):
+        """ Compute my distance from the origin """
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def reflect_x(self):
+        return (self.x, -1*self.y)
+
+
+print(Point(3, 5).reflect_x())
